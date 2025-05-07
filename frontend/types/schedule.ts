fd328532d2d1479225d0f9443759182720541607
Antonio Sasso <antoniosasso00@hotmail.com>
2025-05-07 23:00:19 +0200

    feat(types): aggiunto tipo Schedule per uso in api.ts

---
+++ frontend/types/schedule.ts
@@ -0,0 +1,10 @@
+export type Schedule = {
+    id: number;
+    autoclave_id: number;
+    description?: string;
+    start_time: string;
+    end_time: string;
+    created_at: string;
+    updated_at: string;
+  };
+  
