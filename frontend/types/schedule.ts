f3dacceeed44c10167feaf9af28cfcbd7d1c9586
Antonio Sasso <antoniosasso00@hotmail.com>
2025-05-10 18:06:16 +0200

    Fix: aggiunti campi layout_id e color al tipo Schedule

--- frontend/types/schedule.ts
+++ frontend/types/schedule.ts
@@ -2,6 +2,8 @@ export type Schedule = {
   id: number;
   autoclave_id: number;
   description?: string;
+  layout_id?: string;
+  color?: string;
   start_time: string;
   end_time: string;
   part_ids: number[];
