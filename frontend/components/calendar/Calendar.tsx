fbf1bee9fa81cf00c4da4a4362a0c7359a51bf44
Antonio Sasso <antoniosasso00@hotmail.com>
2025-05-11 17:52:59 +0200

    fix(date-fns): corretti import compatibili con ESM in Calendar

--- frontend/components/calendar/Calendar.tsx
+++ frontend/components/calendar/Calendar.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { Calendar as BigCalendar, dateFnsLocalizer, Event } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
+import { format, parse, startOfWeek, getDay } from "date-fns";
 import it from "date-fns/locale/it";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useEffect, useState } from "react";
