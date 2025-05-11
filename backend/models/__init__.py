0d1687a6aac070accaafa93c92a82cd3a977d5d3
Antonio Sasso <antoniosasso00@hotmail.com>
2025-05-11 12:44:29 +0200

    chore(models): importato modello CureCycle nel modulo __init__

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,6 @@
-from .part import Part
 from .autoclave import Autoclave
-from .schedule import Schedule
 from .catalog_part import CatalogPart
 from .nesting import NestingLayout, NestingPlacement
+from .part import Part
+from .schedule import Schedule
+from .cure_cycle import CureCycle  # 👈 nuovo import
